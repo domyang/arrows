611f9e5bd63a0cb8d9592b81c9785f0c20019de6
Dominic Yang <domyang@ucdavis.edu>
2015-01-17 19:53:51 -0800

    updated arrows.py to completion

--- arrows.py
+++ arrows.py
@@ -10,6 +10,134 @@ v<>^vv<^>vv>v><v^>^^
 ^v<>^<>^>^^^vv^v>>^<
 v>v^^<>><<<^^><^vvv^"""
 
+SAMPLE2 = """>>>>v
+^v<<v
+^vv^v
+^>>v<
+^<<<^"""
+
+SAMPLE3 = """^^v>>v^>>v<<<v>v<>>>>>>>>^vvv^^vvvv<v^^><^^v>
+>><<>vv<><<<^><^<^v^^<vv>>^v<v^vv^^v<><^>><v<
+vv<^v<v<v<vvv>v<v<vv<^<v<<<<<<<<^<><>^><^v>>>
+<v<v^^<v<>v<>v<v<^v^>^<^<<v>^v><^v^>>^^^<><^v
+^>>>^v^v^<>>vvv>v^^<^<<<><>v>>^v<^^<>v>>v<v>^
+^^^<<^<^>>^v>>>>><>>^v<^^^<^^v^v<^<v^><<^<<<>
+v<>v^vv^v<><^>v^vv>^^v^<>v^^^>^>vv<^<<v^<<>^v
+<<<<<^<vv<^><>^^>>>^^^^<^<^v^><^v^v>^vvv>^v^^
+<<v^<v<<^^v<>v>v^<<<<<>^^v<v^>>>v^><v^v<v^^^<
+^^>>^<vv<vv<>v^<^<^^><><^vvvv<<v<^<<^>^>vv^<v
+^^v^>>^>^<vv^^<>>^^v>v>>v>>v^vv<vv^>><>>v<<>>
+^v<^v<v>^^<>>^>^>^^v>v<<<<<>><><^v<^^v><v>^<<
+v>v<><^v<<^^<^>v>^><^><v^><v^^^>><^^<^vv^^^>^
+v><>^><vv^v^^>><>^<^v<^><v>^v^<^<>>^<^vv<v>^v
+><^<v>>v>^<<^>^<^^>v^^v<>>v><<>v<<^><<>^>^v<v
+>vv>^>^v><^^<v^>^>v<^v><>vv>v<^><<<<v^<^vv<>v
+<><<^^>>^<>vv><^^<vv<<^v^v^<^^^^vv<<>^<vvv^vv
+>v<<v^><v<^^><^v^<<<>^<<vvvv^^^v<<v>vv>^>>^<>
+^^^^<^<>^^vvv>v^<<>><^<<v>^<<v>>><>>><<^^>vv>
+<^<^<>vvv^v><<<vvv<>>>>^<<<^vvv>^<<<^vv>v^><^"""
+
+SAMPLE4 = """>v>vvvv<<<^^v^v<^>^<>v<^vv>^^>><v<v^>v<>vv<><>v^<v<><<^v><v<<>^^<v<^^<><v<><v<><>>vv<<>>>^<v<<><>^v<
+>^v><>^v>><<v^<^<>^><><<>^<>>^<<^^^>vv^v><>>>>^^<vvvv>^>^^^><^^vv>^v<<>v^^^>^<>>^v>>v>><>^v<v^^^v>^<
+^v<^v^^>><<>>v><v>v^^^><vv>v>v<>vv^<^^>>^v^<vv>v<>>^v>>^<v>^>^<<><v<v>v><v><v>^v<^^<v>>>>^<<v^>^<v>>
+<^>v>>v>^v>^^v^^v>>^>v<v^v>^<v>><><v^v><^<<v^<>^<<v><^><v<><vv<^v>v<<<>^>^v^^^>v<^^vv^<^^<<^>^v<^><v
+^<<>^<<v<v><<>>>^v<^^<^<><v>>^^><^^v><>>>>>>v>>v<<<<><vvv^<<<^^^v<^v>^<^vv>v<^<<v>^v<<v^v<<vv<^v>><>
+<v<<<>v<^^vv^<<><<v<vv>v^<>vv^<<^<<>vvv>^<^>>><<><v>^v^vv>^v<<^v<^vvv><>v>>>>v<><>><>^^v>^<v>vv<v<v^
+<^v<^><<<^>vvv<<<>vv<^v>>^<^>>><>>^vvv>v><<^<^v<v<<>v><v^>v>^>>vv<>^^^^vv^v>>^^^>>v<<<^v>vv>>>v><><v
+^>><>^<>>>^^^v^>v>v<<<<^^<vv<>v<><^>>>^><>^^vv<>v>vv^v^<><>^><v^^<>v<^>><v^<><<v>^v^v^<^v>>vv^v<v<^>
+<vvv^v<<v><><>>^><<<vvv^<^v>^<v>^>^<>>v>v^vv<<<>v^v>>^v^>^>><<^<v>vv^><v^^<^v>^vv>><<>v^^^<>v^v>v^v>
+vvvv^>>>><>>^^<>^^v^^<v<<>v^<<<<>>>v^^^^<v<^<>>>^^>vv^^^<^<>>^^v<<<<v^^<v^v^^<v<^v^v<v^^<>^^><<vvv>>
+>v<^<v^><<><>>v>>^><<><>v^v>><v<^<>>^>^>>v^<>><>><><<><<<<v>v^v^^<v>>^<v<^>>^<vv^v^<^<><^<<>>>^>v<>^
+^^<^>>>>v^^<>^<>v>>><vvv>><<^>^>^<<v>>^<vv>>^>v<><>vvvvv<^>^>^>>>^v>>vv<vvv>v>><v>^<>^>>><^vv<>^v^^v
+<vv<^vv<^^^>>^^v>v<^^^<<^^<v<<><^^<><^>v^<<vv<<<>v<v^<>v>v<^<v<v>><vvv<^v^><><<^<<v>^v><^<^>^>v><><v
+<^>>>v<<^^^<v<v><vv<<^vv<>><>^v><v<><<v^v^>^>^^^^>^vv<<<<<v>>^vv>^<<<><>>>^v<v<>^>v>v^^<<>><>>>>^>><
+<>^><vv>>v^>><v<>>^^>^><v<^<<<>><<v<<<>>v<<>^^^<v>^>v>^><>vv^^>vv>vv^<><v^<<>vvv<<^v>^^<<v>^vvv^v^<^
+><^<v^<<>vv><<vv^v^v<<<^<v>^vv<>^>^v^^^<<v<>v>><^v<^^v>>v<><<^v><^<<<vv>^>^<>v^v>v<vvv^^<^>^<^v><^vv
+^>^><>v>>v^^<<^^^vv^<^^^>^v<vv^>v<vvv>v^>>>vv>^<^>v^>>>^^^^><v<^<<>v<>v><^><^><v^v<^>^vv^<><><^^<<<^
+<>>^v><><>>>vvv<>^<^^vv^<^>>>>v<^<v<>>><^v>^>^vv^<v<vv<>^^>vvv><><>>v^v^<<^^v<>v<>>^v^<<<v>>^vv^^<>>
+<vvv<v>^v>v>vv<vv^>>^<v^<vvv>^^<>^<v>vv<^<<v^<vv>><vv<<<^v<^^v><v^v>v<>>v>v><v>>v>v<v>>>v^>><v><<<^^
+^><v>^vv^<<v>v<^<^^<<>v<>^>^>><<^^<^<v<>vv<v<<>>>><><>^>>v<v<v<^<<<>v^v^><>><v<^><v>v^<><<v^<^>>>^^^
+v>^<<^><><<>vv^><v<^<>>^^v>^^v<>v^>^>><<vv>^<v<<v>>^vv<>>v<<^^^<>>^v<<v^v<>><<v^<^^vv<^^>v<>^vv^^v<v
+^v^v^>^<>^^>^v>^^>^>>vvvv^>^>>>v>><>vv><v>^>vv><v^><>v^^^v>v^><<<>^<<<v^><^<<>v^^^^>>>>v>><>^<^v^>>>
+><v>v<v>><^>>^<v<^v^>vv><<^v><>>vv>^<<v^v<v<<><<^^>^v>v<<><v^<v^>>v<^v>^v>v^><><v<><<^v>v<<<><^v><v^
+>^>><v><v<v<<^<v<<<v^>^v<>^<<v<v^><<>^^vv^<<><v^>><<<<>><<>^<vv<<<v>><<><<<>>><>^>v>>>^<>>v^<<v^^v^v
+>^v^v>vv>^v<<^^<v>vv><v<><<^v><^>v<^>v<v>^<>v>vvv<>v>v><^<>^<v>><^^<<<v^><<<^v<^<><<>^>^^>v^^vv^><><
+v^^<v<<<v^>v<><<vv^^>^<v><v^^^>^<^^<v<^^v>>^^>^<<>^^>^v^<><>v^<>>><v>^v<^v^v^>><v<v<<<^>v<^v^^>>^>>^
+v>vvv^<^v<^vv<v<v<v<^<<<<v^v<<vv>v>>><^<<v<><vv><v><<<<v<^v<><v^v^>>><>^^^^>>v><^<<><vv>vv^vv^v^^<><
+<^v<<^^<^<<v<<><<^<<>v^>vv^v<^v<>^>vv>>^<><vv><v><<<>^<^^^><<<<v^<>><>v<v^^<v>>^^>^vvv<vv<>^^>v^><>v
+<^^v<<v^^><<vv<>^>^><^v>^^<vvv<vv^^^^vv><vv><>>^<<^v>^<>^^<vv^<><vv^^><v^<^^>^>^<v^v<>><<<v>>vv^<v^v
+><^^v>^v<<v<^>><v^v<^<><<v><^>v^^v<>>vv^^^v<^v<<^^^^>vv<vv^>v><<<><^<v<>^><^v^>^v^>^^<v<^<<<<v^v<v>^
+<v<<^vvv^^v<<^v^<<<<^^^^^v<^^><<<v^>v^<>vvv<<<^vv<>v<^^><vv^vv><vv^v>^^v^^v>^vv<<>vvv>^<>v><^^<><<><
+>^<v<<>v^>^>v><<<>^^^v>^v<^>^v^vvv^v^v<v<^^^^^^v>v^<>v^>vvv^^^>v<<v>v^^v^>>vv^^><^v^^<<^v>^<><vv>^v<
+<vvv<vvvv><^><^<^<^<<>^<>^><v><<^<vv^>vv^^v>v<<v^vv>^vvv^v<<^^<^<>^>v>>>^>>><>^^>>^v>^^v>^v<^<<<<<v^
+^^v>^<>><v>><<v>^v<<vv>^vv^<<v^>>>><<>vvv^^<^v>v>v^v^^^^^^^><^<v<^v>v>v><<^^v>v><>vv>v<<^<<^>v^vvvv>
+vv<vv>>^v<<v^>>^<<^^^>>^>v<>>^^<><>><vv><><v^^^<<>>vvv><<><v>><><><><^^<vv>>^><^^^>>vvv><>>^v^>v>v>v
+<><v<<v^<vv<<^vv<^><^vv>>v<><^<^vvv<<<>v^^>^>v<><<><<>^v>^^<<^^>v^vv>>^^v><vvvv<<<^v^<v<<v><<>v>^>v>
+>^vv^>v^vv>^^v^>vv<^^v>><>><v^^>><v^vvv^v<<<>^<<v^^v^<v>>v<>>v^<>^<>>^vv><^><vvv>>>v>^>^^>v<v^<>>^^^
+<<>^>^>^vv<>><<>^<<^v^vv>^<><<^vv<v^<>^<^>v>v>>v><<^<<>>v<vv^<vv<>v>v^^v<><^>><>v<<^<^<><<>>><>><v>>
+>v<v^v<<>>^v^v>^vvv^><v<>v^v>>vv^^^v>^^>^^>^>^<^vvvv<^<<>^v><v>^vv<>^<^v<>v>v><<<v^<<>>vvv<v^>>><<v<
+^vv<<<>^^<vv^><vv^v^v^><<<><>>>v><^^v>v<><><<>^<<^<v>^><^v>>^><^<^v><v>^^><vv<<<<v<<<vv^<vv<><vv<v<>
+<^>vv>>^v>>v^v>v^^>>>v^<^<v^<^^><^<>^^<v>>^>^^><<<^^^><v^v<><<vv^>v>v>v>>>>^<<<>^v><^<^>^^v>^^<^<v^<
+<v^^>v>v<>^>v<v>^v>>^<<><>>^>v^<vvvvvvv<><^>v>v>^<>^v^^^^^<<>^<<>^^^vvvv^^>^>^>^v>>>^^><^<>^<^vvvv<<
+^vvv^><<<v><><^<><^<>>vvv<v<<v<<v^v^^>><<^<v<^<><>^<^^^v<^v<<^v^vv>>^>v^^<>><>>^^>^<>>v>^^v>>><>v>>^
+v^<v^<v<vvv<<><<>>^v<^><^<^^v^>v<>><^vv<v<v><>^v<^vv<<^^^<v>v<<^<<<<<^v<>^v<v^<<^v<<<v^vvv<^<^>>v^<^
+vvvvv^vv<><^>^^vv^<v<^v<v>>v^^<^><<<>><v>v^<v^^>^<>v>v^^>>>><<^^^^<vvv^<^v^<>>>v>>v^v>><v<^<^^>vvv^>
+^^>v<v^><v<>>^^v<vv>^v><^v>v<<>^<^>>^^>><v^<>^v>><^><><v<^^<^><<><<^^<^^v>v^><<>v>v<^>v<v>^^vv<>v<vv
+<>^v^<>v^<<>^^<^><v<v<><>v^>v<vv>^<^>^>^><vv<^v<>^v<v<<>v^>v^^>>^<>^>v^^<v>>^^>>^><>^>^v>v^<^>v<>^v<
+^<<^>><v>v<>v^>v^<vv^vv^<>v>v<v>v^<^v^vv<^<>^v<>>vvvv^^^>>v<^<>>v^v<^<>>>v<>>^>v<vvvv>v>v>v^v<<v><<^
+^^vv<v>v^^^v><><<v><>><<>v^^>>>^<v<v><<v<>^v><^^v<v<<^v<>>>^^^>^<<v^<>v<v^v>^^<><vv<<<^><><v^<><^<^<
+>v<vvv<v^^<><vv>^>v<vv><<>>^vv>v>v<^v<><>>^<<^<v^^>v^vv<>^^<<^^<^<^v<>>>^v<^><vv^^vv^^v<vv<^v<>^>^<^
+v<^v>v<<v^<v^^v>^^>v^<^vv^v^<^<^>vv<<v<^<v^<<><<><v^<vv^><^>v<^^>v^<^>v<^<<v>v<<><vv^>>v<v<<^>vvv<vv
+^v><^<v>^>^vv><^<<^<>^<^>v^^v>>^>v^^><^^>>^<v^v^><vv^>v<>^^>vv>>^^v>^v<^<<v<vvv>>>^<vv<<>^<^>v^vvv>v
+^>>v<<<<^><^^^>^vv><^<vv<<^><>><>vv<^v^<<>^^v^^v>>^^>^>^^>vv^v^v>^^<vv^v><v><^^<v^>>^v<v>>v^><><^<<^
+<^<>>>vv<>vv^><v^^<vv^><<>><v^v><v^v>^v<<^<v><^v>^v<<v<^<^^<v>vv>^<^><v>>><>v<v>^<vv^^>><^^><<>v<vvv
+vv<><^^<<v>><^^>^^vvv<>^v>v^>^<^^^>v^><^<^^v^^>><v^<><v^v^<<><v><^<>>^vv>^vvv>vv<v<vv>v^<v^<v<v<>vvv
+^vvv>v>vvv^<^<>>><vv<>vvv<<<<<<vv>>^v>vv>v^v>^v>v^<v><>>v^<vv^v^^>v^<v><<<v<<>v>v<<>v>>^^>>^vv<<>><>
+<^v><>^>^v<>v^<>^^<<<<>>>>><<^v^>>><<^v>>>v^v<<<<^^vv<<<^v^<><^^^<>v>v^>^v^>>>v>v<<>><^>v>v^^^<^<vv^
+>v><v>vv<^^v<v^v>v>v>><><^v<>>>v<v<v<<<<vv^>>>>v<<v<v^^<v><^v^v<^^<vv^><>>v>>^vv>>>^v>^<^><^^>>vvv<v
+vv>^>>>^>^>v^vv^<>v^>v^^^v><><><v>><v^^<<v>>v^<>^^v^>>>^>>v>^^>v>v^>vv>>^v^^v>v<>^<><><v^vvv<<<<>>v^
+>^vv>^<^^<>>>vv<<<^^<^^^<<^v<><>^>v>vvv>>^><^^v^>v<>^<<<vv<<<><<vvvvv><vvv<vv>>^>v<vv^vv<<<><vv^vv<>
+v<>v<<><v^>v<<v<^v<^<^^<>v<><^vv><^v^<^<v><<<>v^^>^>>>>v^><<v^^v>^v^><<<v<^^>v<<^v^>v>^^v<>>^^><^<^>
+<^^>^^^^^<^<>>^>>^v^>^^<^><^>^^<<v^^v<vv>>v><<vvv>>v^>v>v>>>^v>^^^>^^^>^v<v^>>>><v<<^<>^v^<<<v^<<<><
+>>><<<^>v>>^<v><^<<^<<><^v^><<vv><<^><<<^vv<>>^><><<v<^^<>>>>>>^>v<^<>>>v>^>v^<<vvv<^<>>>><<^<^v^^<^
+vv<<<<>v<^v><^>>vv><vv<^^>v>^^v<<v^^><<^<^<^^<v>v<v<v>^^v><><^<><^^^>v^<<><^^vv<^<^><^^^>vv<>v>^v^^^
+v>>>^^v>>^v^<<<>vv>^>vvv<^vv>vv><v<<>>vv<vvvvv<<v>>>><<<>>^^v<<^><><^v><><><^<<><^<><v>>v<<^><>^^^<<
+><^^v<v>v><v<v><<<^>v^^<v^^^<^^>v<<v>v^><^<<<vvv^<<vv<^vv>>vv>^vv>^><<<<v<>^<v><<v<v^<<<><<^vv><^<<^
+^^v^<vv><v>>v^v^>v^^<^vv<^<>>><<>^^v<>^<<^>^<>v^<v>^<^<>>^v><^v^^<^><^<<>^v><<>^<<^>>v>v<>v<v<vv^<>>
+>>^^<>vvv>^v^>^v<v>><<><^^v<^v<<<<vvv<<<<>>>>>v<<<>v<<>v<<v><<v^>^v<^^^<v<v>^<<v><<^v>^<^^^>v<v>><<<
+^vv^>><><v>v^v>v>>^>v^>>v^^v>^<^vvv<>><<^^^^vvv^^<v>v^vv<<^>>vv^^v<<^<><<^^>^<>>><><>v^>>>v^^^<<^>v<
+>v>>>vv^<<<v>v>vv>>vv^v<<^^>^^<vvv^>v>v^^vvv>^<v^vv<v><>^<>>v>^><v>vvv^vv>v>v<v><<^>^v<^<<<^^^<vvv<>
+><v^^>>>><vv><^v><>>v>v^v^><<^^vvv<^>^<^^v>><^v^><<v>^vv>v^<v>>v^^>^>^^<<<><^<>^^^>>^v^<v^>^<><>><v<
+>^v><><^^<<^>^v>vv^v<<^^^<^<v>v<>^<^<<v<<^>^>v<>^>v>^<<<>v><<v<>><^vv^vvv>vv>^^><<^v<^vv^^>>^^>v<>>>
+>>v<><vv^vv>>v^v^<<^<<v<vv<^^^^^v^^<<<>^><^^^vvv^v<>^<^<<^>>>vv<v^>>^v<><^>vvv><<v^<<^^><<>>v^v<vv<^
+<v>>^<^v>v>^v^^^><>v^v>v<<v^v<<>^<^<>v^<>^><<^^>><v^^<<>v^^v<^<<><^^v^^v>^<^^v<v^^v^^<<v<^^>>>^><><<
+<v^<><^^<<v^^^vv<><<vv^>>>>^<<v><><^^v<^<vv<<<>v<<^vvvv<>^<v<<<^>^v>vvv^<<v<<v<<<><v>>v^^<><v><<v>>>
+v>^^<^><>^v><vv<>v<^^>>v^^v>>v^<^>>v>^<>^<>vvv>^>>v>^^<>v^>v<^>^^>v^^v^v^<^v>v><<v^<<^><<^>>^^v>^<<^
+v^<v^>><^>^>^^^>>>v^^v^^v^^><<vv^^>^<<v<><^^v<^>>vv^<v>v><^vv^^v^>^>^<^^v<<v<<^^<<v>>>^^^<>>^v<<^v<<
+<vv^><><^^>^^><<<^><^^<>^>vv^^>v^v<<>>^^vv>><^vv>vv^<v^<<v<^v^<>><>^v<^^>^v<>>>v<v<v^^>vv^<>><>><vvv
+v<^<^^v>^><v^>>>>><v><>>^>vv^<<^><v<<<^^^^v<<>v<>vv<>v>>v>vv^v>><v^<<^vv^>><>^<vv>vv>v^v>^v<vv><^vv>
+<<<>^v<>v>v^<v<vv<>v>^>v<>>>>v>>^v^^<v<<^<v^<>^v>vv^vvv>^>>>^^>^^^^^v^^><<><>>^<<^^<><^^^^<v>v<>v><v
+^<>>v^>v^v^>v^>v>v<<vv>v^^v<<v^<v<v><<v>>v<<v^v^<>^vv^<^<^<>^<^vv<v>vv<vv^<v^><^<>^<<<^^^>^<<v<>^^>>
+v>v<v><v^>>v^^<v>^>>^<<>>v^><^>^v^>^<^>vv^<v^^<v>v<^>>^<v>>>>^v<><<<^<>v^>^>>^>v<>^<<<^<><v><^^v^>^>
+^^^v>^>^<^<>><^<v^v^v<>><v^><^^<v>vv<<<<v<^<>^<<>>>v>>^<^>^v^^^>>>v>v<<<v^<<vv><<<<v>^><vv>><v^<^v<>
+^<vv^^<^v<<^^^vv<>v<v^^<<v<v<>v<<<v^^vv<<^v^^>v^><v<>^><^v>^>>^<>vv^<v^<><<v<^<<<>>v<^v^>>v^><v<^<^>
+^>v>>>>^v>vvvvv>^^v^<v>vv>>v<<v^<^>^v<<<>^<<^^v>>>v<><><vv<v^vv>^<v<^v^>>v><<>^<<^v<^^v^v>>v^^><^vvv
+><v>^>v^v<>v>v^v^<>vv><^vvv><>vv>v<v^<^^><<>^vvv<<>>v^^<<vv^<v><<><v<>v<v>^^^^vv^^<<vv>v^^>><v<^^<>v
+^v^v>><><><>><>v<^><v<v><v><^<<^>^^^>^v<<^v^^^<>v<v<>v^^^>>v>^vv>^v<v>>vv<v^>v<v<<vv<<v<^<>v<^^^<<^^
+<<^vv^^v^v<>v^><^><<><>v<^^<<^><>vv>v><v<<>^>^vv^^^^^v^^v>>>v^<^^^><vv>><vv<^>^>vv<vv><><<<>^><v><^<
+v<<v^^^vv^vv<^^^><>>>^^<^<>v<>^^<><>v^^^^v><<^vv>v^^>>v>>>vv^<<>^^v>^^><^<<>v<v>><>^v^<><>^<^v<>vvv>
+<<<v<v>>v^^^><v<>vv^<<<><^^^^>vv<<^>^^vv<>v>^^^>><>v>v<>v^v<>^v>^v<<>><<^<^v<><vv>>^<<>>>>><<<^vv^^>
+^^v<>>><^>>v<>>><v>><v^v<<>v>>v>^v^^>^>>v<>v^^<vv^<^vvv>><v^v^vv^^>v<<<^>><>^^<<<vv><^>^><^v>^^>v^>v
+<^>^^<v^><vvv><v>vvv><>>^v^v^^<v^<^^^v<<v<<>><v<>><^<v>><^<^^^v<<<v<>><^v^^>>>^v<>><^vv><^v^<<<vv<v>
+^^^^<^^v<^<v^><v<^>><^<<<>>>^>^>^^<v^<>^^<v^<>><^>v<><>v^v>>v^v>^>vv>^^v^v^>^<^v<<>>><>v^<v^^>^<v^v>
+^^>>^>^<<<>v<<><^<^v^><>v<<<^vv^v^v>^v^v>>v<^<<v>vv<>vvv^v<v<<^>^v^v^vv>v<<<^vv^>>>^<>^<>^<>>>^<^v>>
+^<<^^>^v^^>>v<vvvv>v^v<<vvv^v>>v><<>vvvvv^^^^<<v><>v>>>>^><^<<<v^<v<>><vv^<>>v^<<v^<^<v^>^>v^><<v^v>
+<<v><>^><>^<>v>^>v>^v^^<<vvv^v^vv>v^^^>^^<>><<><v<<^^^vv<<<>^><>^^vv<v<v><^^>v>^^^vv^<><v^<<v>vv><>v
+<^^^^<^<^^v<>>vv<<v^<vv^<v<^^^^v^v<<>>>^v^<>^v>^^^>^v<v^<><^><>^<vvv><v><<<>><^>^^>>v>^>><<<>v>v<<^v
+vvv>vv^^<v<v^><<v<^>><<vv^^>>v^<v^v><^<<>vv<<><<^^^vv>v><>>v>>vv^^>>vv^<^><vv<^vv>>>^v^<^<><<^<v>^><
+>v<<^>>v^vvv><<^v>><v>>vvv>>>>v>>><<^^^<^^>^^>^v<<^>>v>>><^v^^^^^^<<<<^<>^v><<^<^^vv<<<^><v><^<>^>>v
+<vv>^<<>^>><>^<>^>v<v^^^<v<v>^<v>v>^<v>v^<<>v><<^<>^><^<>^^<^>v<<<>>><^<<<^>>v<<>>^v^^><<v<v<^v^vv^v"""
+
 directions = {
     '^': 'up',
     '>': 'right',
@@ -18,20 +146,27 @@ directions = {
 }
 
 class Path:
-    def __init__(self):
-        pass
+    def __init__(self, cells, grid):
+        self.cells = cells
+        self.grid = grid
+
+    def __str__(self):
+        display = []
+        for row in self.grid.grid:
+            cell_row = []
+            for cell in row:
+                if cell in self.cells:
+                    cell_row.append(cell.direction)
+                else:
+                    cell_row.append(' ')
+            display.append(''.join(cell_row))
+        return '\n'.join(display)
 
     def find_loop(self):
-        pass
-
-class Cell:
-    def __init__(self, x, y, direction):
-        self.x = x
-        self.y = y
-        self.direction = directions[direction]
-
-    def __repr__(self):
-        return "<Cell ({},{}) '{}'>".format(self.x, self.y, self.direction)
+        dup = self.cells[-1]
+        dup_index = self.cells.index(dup)
+        loop = self.cells[dup_index+1:]
+        return Path(loop, self.grid)
 
 
 class Grid:
@@ -40,24 +175,90 @@ class Grid:
         for y, col in enumerate(grid.split('\n')):
             row = []
             for x, char in enumerate(col):
-                row.append(Cell(x, y, char))
+                row.append(Cell(x, y, char, self))
             self.grid.append(row)
 
     def get_cell(self, x, y):
         return self.grid[y][x]
 
     def generate_paths(self):
-        pass
+        paths = []
+        for row in self.grid:
+            for cell in row:
+                paths.append(cell.get_path())
+        return paths
 
     def find_loops(self):
-        pass
+        loops = [path.find_loop() for path in self.generate_paths()]
+        return loops
 
     def __str__(self):
-        return str(self.grid)
+        display = []
+        for row in self.grid:
+            cell_row = []
+            for cell in row:
+                cell_row.append(cell.direction)
+            display.append(''.join(cell_row))
+        return '\n'.join(display)
 
     def find_largest_loop(self):
-        pass
+        return max(self.find_loops(), key=(lambda x: len(x.cells)))
+
+class Cell:
+    def __init__(self, x, y, direction, grid=None):
+        self.x = x
+        self.y = y
+        self.direction = direction
+        self.grid = grid
+
+    def __repr__(self):
+        return "<Cell ({},{}) '{}'>".format(self.x, self.y, directions[self.direction])
+
+    def get_next_cell(self):
+        if self.direction == '^':
+            return self.grid.get_cell(self.x, self.y - 1)
+        elif self.direction == '<':
+            return self.grid.get_cell(self.x - 1, self.y)
+        elif self.direction == '>':
+            try:
+                return self.grid.get_cell(self.x + 1, self.y)
+            except IndexError:      # if tries to index past end of row
+                return self.grid.get_cell(0, self.y)
+        elif self.direction == 'v':
+            try:
+                return self.grid.get_cell(self.x, self.y + 1)
+            except IndexError:      # if tries to index past end of col
+                return self.grid.get_cell(self.x, 0)
+
+    def get_path(self):
+        cell_list = [self]
+        cell = self
+        while cell.get_next_cell() not in cell_list:
+            cell = cell.get_next_cell()
+            cell_list.append(cell)
+        cell_list.append(cell.get_next_cell())
+        path = Path(cell_list, self.grid)
+        return path
+
 
 if __name__ == '__main__':
-    grid = Grid(SAMPLE)
-    print(grid.get_cell(5, 5))
+    import time
+    grid = Grid(SAMPLE3)
+    txt = open('path-display.txt', 'a')
+    """
+    paths = grid.generate_paths()
+    cell = grid.get_cell(19, 10)
+    path = cell.get_path()
+    print(path)
+    print(path.find_loop())
+    """
+    
+    for i, path in enumerate(grid.generate_paths()):
+        print("cell {}".format(i))
+        print(path)
+        print(path.find_loop())
+        time.sleep(1)
+    
+    loop = grid.find_largest_loop()
+    print(loop)
+    print(len(loop.cells))
